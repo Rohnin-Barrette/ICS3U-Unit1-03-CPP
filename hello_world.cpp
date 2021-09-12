688944fc3c9e16832a93d9fc57ef32c73cd696a0
Rohnin Barrete <rohnin.barrette@mths.ca>
2021-09-12 17:12:37 +0000

    made a program where cpp says hello to the world
    Signed-off-by: Rohnin Barrete <rohnin.barrette@mths.ca>

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Rohnin Barrette All rights reserved
+//
+// Created by:Rohnin Barrette
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!";
+}
